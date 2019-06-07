44b8fb6635249c5c22e43725fa2bbfca8dcb600e
aks16588 <--replace-all>
2019-06-07 10:17:42 +0530

    main code logic changes - all xpath

--- src/WhatsappToContact.py
+++ src/WhatsappToContact.py
@@ -6,71 +6,70 @@ from selenium.webdriver.common.by import By
 import time
 import sys
 
-# Replace below path with the absolute path
-# to chromedriver in your computer
+
 class WhatsappMessage:
-	"""
-	class for sending message via Whatsappweb
-	"""
-	def __init__(self, friends_lists, message):
-		self.friends_lists = friends_lists
-		self.message = message
+    """
+    class for sending message via Whatsappweb
+    """
 
-	def send_message(self):
-		"""
-		Method to send messages
-		"""
-		# friends_lists = ['Save']
+    def __init__(self, friends_lists, message, chromedriver_path, url="https://web.whatsapp.com/"):
+        self.friends_lists = friends_lists
+        self.message = message
+        self.chromedriver_path = chromedriver_path
+        self.url = url
 
-		# driver = webdriver.Chrome('../chromedriver')
-		driver = webdriver.Chrome(executable_path='C:\whatsapp-automate\src\chromedriver.exe')
+    def send_message(self):
+        "Method to send messages "
+        driver = webdriver.Chrome(
+            executable_path=self.chromedriver_path)
+        driver.maximize_window()
+        driver.get(self.url)
+        wait = WebDriverWait(driver, 600)
+        group_title = wait.until(
+            EC.visibility_of_element_located((By.XPATH, "//*[@id='side']")))
+        search = driver.find_elements_by_xpath(
+            '//*[@id="side"]/div/div/label/input')[0]
+        for friend in self.friends_lists:
+            try:
+                wait = WebDriverWait(driver, 5)
+                search.clear()
+                search.send_keys(friend)
+                x_arg = f'//span[@title="{friend}"]'
+                try:
+                    group_title = wait.until(EC.presence_of_element_located((
+                        By.XPATH, x_arg)))
+                except Exception as e:
+                    print(f"{friend} not identified!", e)
+                else:
+                    group_title.click()
+                    message = driver.find_elements_by_xpath(
+                        '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+                    message.send_keys(self.message)
 
-		url = driver.command_executor._url
-		session_id = driver.session_id
-
-
-		try:
-			with open("url_session_id.txt", "r") as f:
-				url, session_id = f.read().split()
-				driver = webdriver.Remote(command_executor=url,desired_capabilities={})
-				driver.session_id = session_id
-
-		except FileNotFoundError:
-			with open("url_session_id.txt", "w") as f:
-				f.write(f"{url}\n{session_id}")
-
-		driver.get("https://web.whatsapp.com/")
-		wait = WebDriverWait(driver, 600)
-
-		group_title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.RLfQR")))
-		search = driver.find_elements_by_xpath('//*[@id="side"]/div/div/label/input')[0]
-		print (search)
-		for friend in self.friends_lists:
-			search.clear()
-			search.send_keys(friend)
-			wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button._3Burg")))
-			time.sleep(3)
-			persons = driver.find_elements_by_class_name('_2wP_Y')
-			print(len(persons))
-			for person in persons:
-				try:
-					person_title = person.find_element_by_class_name('_1wjpf')
-					print(person_title.get_attribute("title"))
-					if person_title.get_attribute("title") == friend:
-						person_contact = person.find_element_by_class_name('_2EXPL')
-						person_contact.click()
-						message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
-						message.send_keys("Automated Hello message")
-
-						sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
-						sendbutton.click()
-						break
-				except:
-					print("*")
-					continue
-
-		driver.close()
+                    try:
+                        wait = WebDriverWait(driver, 2)
+                        x_arg = '//*[@id="main"]/footer/div[1]/div[3]/button'
+                        sendbutton = wait.until(EC.presence_of_element_located((
+                            By.XPATH, x_arg)))
+                    except Exception as e2:
+                        print (e2)
+                    else:
+                        pass
+                        # sendbutton.click()
+            except Exception as e1:
+                print ("final", e1)
+                
+        # driver.close()
 
 
-obj_whatsapp = WhatsappMessage(["Save"], "Thanks")
+# check for group names with icons
+# check for unchatted contact
+
+whatsapp_web_url = "https://web.whatsapp.com/"
+chromedriver_path = 'C:\whatsapp-automate\src\chromedriver.exe'
+friends_lists = ["Himanshi Swain", "No Title", "Save"]
+message = "Test ping one more"
+
+obj_whatsapp = WhatsappMessage(
+    friends_lists, message, chromedriver_path, whatsapp_web_url)
 obj_whatsapp.send_message()
